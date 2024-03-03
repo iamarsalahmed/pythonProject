3d696167546eb650740ca090b5597f515609f5fd
iamarsalahmed <65033558+iamarsalahmed@users.noreply.github.com>
2024-03-03 14:26:24 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+# import cv2
+# import mediapipe as mp
+# import pyautogui
+# cap = cv2.VideoCapture(0)
+# hand_detector = mp.solutions.hands.Hands()
+# drawing_utils = mp.solutions.drawing_utils
+# screen_width, screen_height = pyautogui.size()
+# index_y = 0
+# while True:
+#     _, frame = cap.read()
+#     frame = cv2.flip(frame, 1)
+#     frame_height, frame_width, _ = frame.shape
+#     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     output = hand_detector.process(rgb_frame)
+#     hands = output.multi_hand_landmarks
+#     if hands:
+#         for hand in hands:
+#             drawing_utils.draw_landmarks(frame, hand)
+#             landmarks = hand.landmark
+#             for id, landmark in enumerate(landmarks):
+#                 if id == 8:
+#                     x = int(landmark.x*frame_width)
+#                     y = int(landmark.y*frame_height)
+#                     if id ==8:
+#                         cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+#                         index_x = screen_width/frame_width*x
+#                         index_y = screen_height/frame_height*y
+#                         pyautogui.moveTo(index_x, index_y)
+#                     if id == 4:
+#                         cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+#                         thumb_x = screen_width/frame_width*x
+#                         thumb_y = screen_height/frame_height*y
+#                         # print('outside ', abs(index_y - thumb_y))
+#                         # if abs(index_y - thumb_y) < 20:
+#                         #     print('Click')
+#     cv2.imshow('virtual mouse', frame)
+#     cv2.waitKey(1)
+#
+import cv2
+import mediapipe as mp
+import pyautogui
+
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
+
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame_height, frame_width, _ = frame.shape
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+
+            # Process index finger
+            index_finger_landmark = landmarks[8]
+            index_x = int(index_finger_landmark.x * frame_width)
+            index_y = int(index_finger_landmark.y * frame_height)
+            cv2.circle(img=frame, center=(index_x, index_y), radius=10, color=(0, 255, 255))
+            pyautogui.moveTo(screen_width / frame_width * index_x, screen_height / frame_height * index_y)
+
+            # Process thumb
+            thumb_landmark = landmarks[4]
+            thumb_x = int(thumb_landmark.x * frame_width)
+            thumb_y = int(thumb_landmark.y * frame_height)
+            cv2.circle(img=frame, center=(thumb_x, thumb_y), radius=10, color=(0, 255, 255))
+            if abs(index_y - thumb_y) < 20:
+                pyautogui.click()
+                pyautogui.sleep(1)
+
+    cv2.imshow('virtual mouse', frame)
+    cv2.waitKey(1)
+
